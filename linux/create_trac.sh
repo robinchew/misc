78bbc1555754d0f12092ddeb900a872c14315ab4
Robin Chew <robin.chew@empiriamedia.com>
2009-09-14 02:02:29 +0000

    boot log, start script in init.d, modified create_trac, change default text editor

--- linux/create_trac.sh
+++ linux/create_trac.sh
@@ -5,34 +5,36 @@ sudo trac-admin $1 initenv
 sudo chown -R www-data:$2 $1
 sudo chmod -R g+w $1
 
-echo '
-[components]
-acct_mgr.admin.accountmanageradminpage = enabled
-acct_mgr.api.accountmanager = enabled
-acct_mgr.db.sessionstore = enabled
-acct_mgr.htfile.abstractpasswordfilestore = enabled
-acct_mgr.htfile.htdigeststore = enabled
-acct_mgr.htfile.htpasswdstore = enabled
-acct_mgr.http.httpauthstore = enabled
-acct_mgr.notification.accountchangelistener = enabled
-acct_mgr.notification.accountchangenotificationadminpanel = enabled
-acct_mgr.pwhash.htdigesthashmethod = enabled
-acct_mgr.pwhash.htpasswdhashmethod = enabled
-acct_mgr.svnserve.svnservepasswordstore = enabled
-acct_mgr.web_ui.accountmodule = enabled
-acct_mgr.web_ui.emailverificationmodule = disabled
-acct_mgr.web_ui.loginmodule = enabled
-acct_mgr.web_ui.registrationmodule = enabled
-permredirect.filter.permredirectmodule = disabled
-trac.web.auth.loginmodule = disabled
-tracsectionedit.web_ui.wikisectioneditmodule = enabled
-tracwysiwyg.templateprovider = enabled
-tracwysiwyg.wysiwygwikifilter = enabled
-autocompleteusers.* = enabled
-' | sudo tee -a $1/conf/trac.ini
+grep '\[components\]' $1/conf/trac.ini
+if [ $? != 0 ];then
+    echo '
+    [components]
+    acct_mgr.admin.accountmanageradminpage = enabled
+    acct_mgr.api.accountmanager = enabled
+    acct_mgr.db.sessionstore = enabled
+    acct_mgr.htfile.abstractpasswordfilestore = enabled
+    acct_mgr.htfile.htdigeststore = enabled
+    acct_mgr.htfile.htpasswdstore = enabled
+    acct_mgr.http.httpauthstore = enabled
+    acct_mgr.notification.accountchangelistener = enabled
+    acct_mgr.notification.accountchangenotificationadminpanel = enabled
+    acct_mgr.pwhash.htdigesthashmethod = enabled
+    acct_mgr.pwhash.htpasswdhashmethod = enabled
+    acct_mgr.svnserve.svnservepasswordstore = enabled
+    acct_mgr.web_ui.accountmodule = enabled
+    acct_mgr.web_ui.emailverificationmodule = disabled
+    acct_mgr.web_ui.loginmodule = enabled
+    acct_mgr.web_ui.registrationmodule = enabled
+    permredirect.filter.permredirectmodule = disabled
+    trac.web.auth.loginmodule = disabled
+    tracsectionedit.web_ui.wikisectioneditmodule = enabled
+    tracwysiwyg.templateprovider = enabled
+    tracwysiwyg.wysiwygwikifilter = enabled
+    autocompleteusers.* = enabled
+    ' | sudo tee -a $1/conf/trac.ini
 
-
-sudo perl -i -pe 's:\[account-manager\]:$&\npassword_file = /etc/apache2/htpasswd/dav_svn.passwd\npassword_store = HtPasswdStore:' $1/conf/trac.ini 
+    sudo perl -i -pe 's:\[account-manager\]:$&\npassword_file = /etc/apache2/htpasswd/dav_svn.passwd\npassword_store = HtPasswdStore:' $1/conf/trac.ini 
+fi
 
 sudo perl -i -pe 's:account_changes_notify_addresses.*$:account_changes_notify_addresses = robinchew\@gmail.com:' $1/conf/trac.ini
 
@@ -49,4 +51,3 @@ sudo perl -i -pe 's:max_size.*$:max_size = 26214400:g' $1/conf/trac.ini
 sudo perl -i -pe 's:always_notify_owner.*$:always_notify_owner = true:g' $1/conf/trac.ini
 
 sudo perl -i -pe 's:always_notify_reporter.*$:always_notify_reporter = true:g' $1/conf/trac.ini
-
---
+++ linux/init.d
@@ -0,0 +1,9 @@
+create script in /etc/init.d/script
+sudo chmod 755 /etc/init.d/script
+sudo update-rc.d script defaults
+OR
+sudo update-rc.d script defaults 99
+
+man update-rc.d
+
+http://ubuntuforums.org/showpost.php?p=1123972&postcount=8
